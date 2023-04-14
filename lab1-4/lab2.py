f6ff37686f6005f46495a0627a5a0e041a2af8d7
KateZin <90635745+KateZin@users.noreply.github.com>
2023-04-14 14:42:27 +0300

    fix lab 2

--- lab1-4/lab2.py
+++ lab1-4/lab2.py
@@ -57,26 +57,76 @@ def count_table_res():
                 z_r.append(count_z_r(sample, size))
                 z_q.append(count_z_q(sample))
                 z_tr.append(count_z_tr(sample, size))
+
+                E_mean = np.around(np.mean(mean), decimals=4)
+                E_mediana = np.around(np.mean(median), decimals=4)
+                E_z_r = np.around(np.mean(z_r), decimals=4)
+                E_z_Q = np.around(np.mean(z_q), decimals=4)
+                E_z_tr = np.around(np.mean(z_tr), decimals=4)
+
+                D_mean = np.around(np.mean(np.multiply(mean, mean)) - np.mean(mean) * np.mean(mean), decimals=4)
+                D_mediana = np.around(np.mean(np.multiply(median, median)) - np.mean(median) * np.mean(median), decimals=4)
+                D_z_r = np.around(np.mean(np.multiply(z_r, z_r)) - np.mean(z_r) * np.mean(z_r), decimals=4)
+                D_z_Q = np.around(np.mean(np.multiply(z_q, z_q)) - np.mean(z_q) * np.mean(z_q), decimals=4)
+                D_z_tr = np.around(np.mean(np.multiply(z_tr, z_tr)) - np.mean(z_tr) * np.mean(z_tr), decimals=4)
+
             with open("lab2_res/" + dist + str(size) + ".tex", "w") as f:
                 f.write("\\begin{tabular}{|c|c|c|c|c|c|}\n")
                 f.write("\\hline\n")
                 f.write("& \\bar{x} & mediana & z_r & z_Q & z_tr & \\\\\n")
-                f.write("E(z) & " + f"{np.around(np.mean(mean), decimals=4)} & "
-                                    f"{np.around(np.mean(median), decimals=4)} & "
-                                    f"{np.around(np.mean(z_r), decimals=4)} & "
-                                    f"{np.around(np.mean(z_q), decimals=4)} & "
-                                    f"{np.around(np.mean(z_tr), decimals=4)} & \\\\\n")
+                f.write("\\hline\n")
+                f.write("E(z) & " + f"{E_mean} & "
+                                    f"{E_mediana} & "
+                                    f"{E_z_r} & "
+                                    f"{E_z_Q} & "
+                                    f"{E_z_tr} & \\\\\n")
                 f.write("\\hline\n")
                 f.write(
-                    "D(z) & " + f"{np.around(np.mean(np.multiply(mean, mean)) - np.mean(mean) * np.mean(mean), decimals=4)} & "
-                                f"{np.around(np.mean(np.multiply(median, median)) - np.mean(median) * np.mean(median), decimals=4)} & "
-                                f"{np.around(np.mean(np.multiply(z_r, z_r)) - np.mean(z_r) * np.mean(z_r), decimals=4)} & "
-                                f"{np.around(np.mean(np.multiply(z_q, z_q)) - np.mean(z_q) * np.mean(z_q), decimals=4)} & "
-                                f"{np.around(np.mean(np.multiply(z_tr, z_tr)) - np.mean(z_tr) * np.mean(z_tr), decimals=4)} & \\\\\n")
+                    "D(z) & " + f"{D_mean} & "
+                                f"{D_mediana} & "
+                                f"{D_z_r} & "
+                                f"{D_z_Q} & "
+                                f"{D_z_tr} & \\\\\n")
+                f.write("\\hline\n")
+
+                mean_left, mean_right = np.around(E_mean - sqrt(D_mean), decimals=1), np.around(E_mean + sqrt(D_mean), decimals=1)
+                mediana_left, mediana_right = np.around(E_mediana - sqrt(D_mediana), decimals=1), np.around(E_mediana + sqrt(D_mediana), decimals = 1)
+                z_r_left, z_r_right = np.around(E_z_r - sqrt(D_z_r), decimals=1), np.around(E_z_r + sqrt(D_z_r), decimals = 1)
+                z_q_left, z_q_right = np.around(E_z_Q - sqrt(D_z_Q), decimals=1), np.around(E_z_Q + sqrt(D_z_Q), decimals=1)
+                z_tr_left, z_tr_right = np.around(E_z_tr - sqrt(D_z_tr), decimals=1), np.around(E_z_tr + sqrt(D_z_tr), decimals=1)
+
+                f.write(
+                    "E(x) - \sqrt(D(z)) & " + f"{mean_left} & "
+                                              f"{mediana_left} & "
+                                              f"{z_r_left} & "
+                                              f"{z_q_left} & "
+                                              f"{z_tr_left} & \\\\\n")
+
+                f.write("\\hline\n")
+                f.write(
+                    "E(x) + \sqrt(D(z)) & " + f"{mean_right} & "
+                                f"{mediana_right} & "
+                                f"{z_r_right} & "
+                                f"{z_q_right} & "
+                                f"{z_tr_right} & \\\\\n")
+                f.write("\\hline\n")
+
+
+                mean_point = round((mean_left + mean_right)/2)
+                mediana_point = round((mediana_left + mediana_right)/2)
+                z_r_point = round((z_r_left + z_r_right)/2)
+                z_q_point = round((z_q_left + z_q_right)/2)
+                z_tr_point = round((z_tr_left + z_tr_right)/2)
+
+                f.write(
+                    "\n$\hat{E}$ & " + f"{mean_point} & "
+                                              f"{mediana_point} & "
+                                              f"{z_r_point} & "
+                                              f"{z_q_point} & "
+                                              f"{z_tr_point} & \\\\\n")
+
+                f.write("\\hline\n")
                 f.write("\\end{tabular}")
-
-
-
 
 def run_lab2():
     for dist in ds.distributions:
